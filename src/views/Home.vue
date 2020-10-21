ee9d4722da8ece6b0ccf4120d909defd228a7300
Evan Ang <alethiologist@gmail.com>
2020-10-22 01:51:35 +0800

    created gitignore file

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>This is the landing page</div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/Riddles.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>This is for riddles</div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
